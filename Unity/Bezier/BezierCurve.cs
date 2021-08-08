a3683cd1657bccd7542966b5919013c9ac0b78df
Łukasz Kraśniewski <lukasz.kra@outlook.com>
2021-08-08 20:40:08 +0200

    Bezier curve

---
+++ Unity/Bezier/BezierCurve.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace ProceduralLevel.UnityPlugins.Animation.Unity
+{
+	[Serializable]
+	public class BezierCurve
+	{
+		private readonly List<Vector3> m_Buffer = new List<Vector3>();
+
+		public List<Vector3> ControlPoints = new List<Vector3>();
+
+		public BezierCurve Add(Vector3 controlPoint)
+		{
+			ControlPoints.Add(controlPoint);
+			return this;
+		}
+
+		public Tween LocalMoveTween(Transform transform, float duration)
+		{
+			return Tween((position) => transform.localPosition = position, duration);
+		}
+
+		public Tween Tween(Action<Vector3> setter, float duration)
+		{
+			float t = 0f;
+			return DOTween.To(() => t,
+			(newT) =>
+			{
+				t = newT;
+				Vector3 position = Evaluate(t);
+				setter(position);
+			}, 1f, duration);
+		}
+
+		public Vector3 Evaluate(float t)
+		{
+			if(ControlPoints.Count == 0)
+			{
+				throw new NotSupportedException();
+			}
+			m_Buffer.Clear();
+			m_Buffer.AddRange(ControlPoints);
+			return InterpolateLayer(t);
+		}
+
+		private Vector3 InterpolateLayer(float t)
+		{
+			int count = m_Buffer.Count;
+			if(count == 1)
+			{
+				return m_Buffer[0];
+			}
+			int lastElement = count-1;
+			for(int x = 0; x < lastElement; ++x)
+			{
+				Vector3 a = m_Buffer[x];
+				Vector3 b = m_Buffer[x+1];
+				m_Buffer[x] = Interpolate(a, b, t);
+			}
+			m_Buffer.RemoveAt(lastElement);
+			return InterpolateLayer(t);
+		}
+
+		private Vector3 Interpolate(Vector3 a, Vector3 b, float t)
+		{
+			Vector3 vectorDelta = b-a;
+			return a+vectorDelta*t;
+		}
+	}
+}
